8b9d464705076efd9ff3609fef0c700c6a2ace68
Saimanish7 <saimanishredishetty@gmail.com>
2025-07-22 12:10:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+const root=getElementsId("root")
+function App(){
+    return(
+         <>
+         <main>
+            <h1>URL Shortner Web app</h1>
+
+         </main>
+         </>
+    )
+}
+root.render (App);
